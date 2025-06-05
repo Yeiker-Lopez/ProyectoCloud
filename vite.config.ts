68cad12f0f546708cf340ed6f136ecd5a5174a82
Yeiker-Lopez <e0962869434@live.uleam.edu.ec>
2025-06-05 13:29:43 -0500

    port

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 8079, // <--- Aquí cambias el puerto deseado
+    port: 3000, // <--- Aquí cambias el puerto deseado
   },
 })
